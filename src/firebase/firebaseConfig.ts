31adc55c16139f72fbd052cc0075b691598665be
jinseok9338 <jinseok9338@gmail.com>
2021-10-24 10:29:46 +0000

    update

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -1,10 +1,3 @@
 export const FirebaseConfig = {
-  apiKey: "AIzaSyAiL4K7sJL-qbVwYeLY4DLBzZmA1GyzMIE",
-  authDomain: "chat-app-a9c9c.firebaseapp.com",
-  databaseURL: "https://chat-app-a9c9c.firebaseio.com",
-  projectId: "chat-app-a9c9c",
-  storageBucket: "chat-app-a9c9c.appspot.com",
-  messagingSenderId: "660633902974",
-  appId: "1:660633902974:web:a6a0458f9a1b541da39d2f",
-  measurementId: "G-BJ188X4KE3",
+ // your Firebase config
 };
